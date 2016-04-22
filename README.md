9b48c8f00d8c3fdbeea8ea02477d607c25f67e78
Eric Normand <ericwnormand@gmail.com>
2016-04-22 16:11:57 -0500

    Ready for release.

---
+++ README.md
@@ -0,0 +1,18 @@
+# a-game-of-json
+
+A Clojure exploration of *Game of Thrones* character data.
+
+This code was used in the *A Game of JSON* lesson in the
+[*PurelyFunctional.tv Online Mentoring*][ment] program.
+
+[ment]: http://www.purelyfunctional.tv/
+
+## License
+
+[![CC0](http://i.creativecommons.org/p/zero/1.0/88x31.png)](http://creativecommons.org/publicdomain/zero/1.0/)
+
+To the extent possible under law, the person who associated CC0 with
+this work has waived all copyright and related or neighboring rights
+to the code in this repository.
+
+See the `LICENSE` file for more information.
---
+++ src/a_game_of_json/core.clj
@@ -0,0 +1,47 @@
+(ns a-game-of-json.core
+  (:require [cheshire.core :as json]))
+
+(defonce books-raw (slurp "https://raw.githubusercontent.com/joakimskoog/AnApiOfIceAndFire/master/AnApiOfIceAndFire.Data.Feeder/Data/books.json"))
+
+(defonce characters-raw (slurp "https://raw.githubusercontent.com/joakimskoog/AnApiOfIceAndFire/master/AnApiOfIceAndFire.Data.Feeder/Data/characters.json"))
+
+(defonce houses-raw (slurp "https://raw.githubusercontent.com/joakimskoog/AnApiOfIceAndFire/master/AnApiOfIceAndFire.Data.Feeder/Data/houses.json"))
+
+(defonce books-json (json/parse-string books-raw))
+
+(defonce chars-json (json/parse-string characters-raw))
+
+(defonce houses-json (json/parse-string houses-raw))
+
+(defonce books-by-id (into {} (for [book books-json]
+                                [(get book "Id") book])))
+
+(defonce chars-by-id (into {} (for [char chars-json]
+                                [(get char "Id") char])))
+
+(defonce houses-by-id (into {} (for [houses houses-json]
+                                 [(get houses "Id") houses])))
+
+(defn children [char]
+  (map #(get chars-by-id %) (get char "Children")))
+
+(defn grandchildren [char]
+  (mapcat children (children char)))
+
+(defn print-children []
+  (doseq [char chars-json]
+    (let [children (children char)]
+      (when (seq children)
+        (println (get char "Name"))
+        (doseq [child children]
+          (println "  " (get child "Name")))
+        (println "----")))))
+
+(defn print-grandchildren []
+  (doseq [char chars-json]
+    (let [grandchildren (grandchildren char)]
+      (when (seq grandchildren)
+        (println (get char "Name"))
+        (doseq [gchild grandchildren]
+          (println "  " (get gchild "Name")))
+        (println "----")))))
